7980c401a9a84decd53499f7c88862eebd2ad54e
Chad <chad@pixelatedempathy.com>
2025-09-13 08:36:42 -0400

    fix: update CDN asset map return statement and add Sentry plugin to Vite config; add alias for cipher mock in Vitest config

--- server.js
+++ server.js
@@ -10,3 +10,5 @@ app.get('/', (req, res) => res.send('Pixelated Empathy Home'));
 
 app.listen(PORT, () => console.log(`pixelated-app listening on port ${PORT}`));
   
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="93104627-6895-5eb0-96a4-0c009e1915b5")}catch(e){}}();
+//# debugId=93104627-6895-5eb0-96a4-0c009e1915b5
