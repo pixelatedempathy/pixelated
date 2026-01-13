dce86f3d3fb4b9224bd443b572edd66f0d973f63
Chad <chad@gemcity.xyz>
2026-01-13 01:32:08 -0500

    Remove unused dependency files for Astro React and RSS.
    Deleted `.vite` dependency files for unused `@astrojs/react` and `@astrojs/rss`. This change ensures the project remains clean and avoids bundling unnecessary code.

--- .vite/deps/module-QK64VTW3.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_module
-} from "./chunk-PBWNZDMX.js";
-import "./chunk-PLDDJCW6.js";
-export default require_module();
--- .vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-I2XJTFW5.js";
-import "./chunk-CHY6K7RD.js";
-import "./chunk-VZKCEW2Y.js";
-import "./chunk-PLDDJCW6.js";
-export default require_client();
