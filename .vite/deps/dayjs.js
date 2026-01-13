dae89b045965a51b2d9e0bab55da52cb64fe7f86
Chad <chad@gemcity.xyz>
2026-01-13 01:32:08 -0500

    Remove unused dependency files for Astro React and RSS.
    Deleted `.vite` dependency files for unused `@astrojs/react` and `@astrojs/rss`. This change ensures the project remains clean and avoids bundling unnecessary code.

--- .vite/deps/dayjs.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_dayjs_min
-} from "./chunk-EXZZPVWN.js";
-import "./chunk-PLDDJCW6.js";
-export default require_dayjs_min();
--- .vite/deps/fs-6AIMAK65.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_fs
-} from "./chunk-XP2EKV2G.js";
-import "./chunk-PLDDJCW6.js";
-export default require_fs();
