b6f04207aef7666007b3bedfb30f0a470ba26fc9
Chad <chad@gemcity.xyz>
2026-01-28 06:39:12 -0500

    chore: squash merge staging into master

--- src/client-entry.js
+++ src/client-entry.js
@@ -1,10 +1,1 @@
-/**
- * Client entry point
- * This file loads polyfills before all other client-side code
- */
-
-// No Node.js built-in polyfills should be present in Astro client-side code.
-// If you need browser API polyfills, add them here explicitly.
-
-// No exports needed for client entry
-// This file simply sets up the environment
+export {}
